e8e2502014a3d731c1ae86b01233deff1a1bc5cd
dury <dury@adifo.com>
2019-10-01 09:13:59 +0200

    first upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+window.addEventListener('load', Initieer);
+
+// Globale Variabelen
+
+
+// StartUP
+function Initieer() {
+  // Lokale variabelen
+
+  // Inlezen DOM-elementen
+
+
+  // Toevoegen Event-Listeners
+
+
+}
+
+function ToonVerbergElementViaId() {
+
+}
+
+function ToonVerbergElementViaSelector() {
+
+}
+
+
