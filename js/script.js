17ce0e8c6a3df1d3cca351e021e3a86a105d91df
dury <dury@adifo.com>
2019-10-01 10:44:07 +0200

    js changes

--- js/script.js
+++ js/script.js
@@ -2,26 +2,41 @@
 window.addEventListener('load', Initieer);
 
 // Globale Variabelen
+var divFeedback;
+var btnVerbergElementMetId;
+var btnVerbergElementMetSelector;
 
 
 // StartUP
 function Initieer() {
-  // Lokale variabelen
+    // Lokale variabelen
+    var txtId;
+    var txtNaam;
+    var txtCssSelector;
 
-  // Inlezen DOM-elementen
+    // Inlezen DOM-elementen
+    txtId = document.getElementById("txtId");
+    txtNaam = document.getElementById("txtNaam");
+    txtCssSelector = document.getElementById("txtCssSelector");
+    txtId.value = "TEST";
 
 
-  // Toevoegen Event-Listeners
+    // Toevoegen Event-Listeners
+    btnVerbergElementMetId.addEventListener("click", ToonVerbergElementViaId);
+    btnVerbergElementMetSelector.addEventListener("click", ToonVerbergElementViaSelector);
+
+}
+
+function ToonVerbergElementViaId()
+{
 
 
 }
 
-function ToonVerbergElementViaId() {
+function ToonVerbergElementViaSelector()
+{
 
 }
 
-function ToonVerbergElementViaSelector() {
-
-}
 
 
